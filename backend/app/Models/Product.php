b40d20cdecf93aa3a2c6d2afcb8103a884eb5412
Michael Gelvez <gelvezmichael@yahoo.com>
2025-06-17 09:21:36 +0800

    feat(backend): Add image column on products table

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -20,6 +20,7 @@ class Product extends Model
      */
     protected $fillable = [
         'type_id',
+        'image',
         'name',
         'description',
         'price',
--- backend/database/migrations/2025_06_16_141803_create_products_table.php
+++ backend/database/migrations/2025_06_16_141803_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('type_id')->constrained('product_types');
+            $table->string('image')->nullable();
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
