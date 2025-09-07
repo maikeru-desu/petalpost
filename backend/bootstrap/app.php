7333cf9f4b05815227637e06b7dc79a9299e712e
Michael Gelvez <gelvezmichael@yahoo.com>
2025-09-08 07:44:49 +0800

    feat: Add Admin Route File

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->namespace('admin')
+                ->prefix('api/admin')
+                ->name('admin.')
+                ->group(base_path('routes/admin-user-api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
@@ -20,6 +28,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => App\Http\Middleware\EnsureEmailIsVerified::class,
+            'role' => Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
 
         //
